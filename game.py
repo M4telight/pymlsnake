5125141e0c85885c1525be2b8f2857988592dec1
Christian Bartz <christianbartz@gmail.com>
2017-04-06 21:37:06 +0200

    add cofiguration options

--- game.py
+++ game.py
@@ -29,12 +29,12 @@ class Game(object):
     """
     The main game class that holds the gameloop.
     """
-    def __init__(self, mlhost, mlport):
+    def __init__(self, mlhost, mlport, screen_width, screen_height):
         """
         Create a screen and define some game specific things.
         """
         pymlgame.init()
-        self.screen = pymlgame.Screen(mlhost, mlport, 40, 16)
+        self.screen = pymlgame.Screen(mlhost, mlport, screen_width, screen_height)
         self.clock = pymlgame.Clock(15)
 
         part = (int(self.screen.width / 2), int(self.screen.height / 2))
@@ -178,6 +178,20 @@ class Game(object):
 
 
 if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Play Snake on your Matelight")
+    parser.add_argument('host', help='remote host to connect to')
+    parser.add_argument('-p', '--port', type=int, default=1337, help='remote port')
+    parser.add_argument('--width', type=int, default=15, help='width of matelight')
+    parser.add_argument('--height', type=int, default=16, help='height of matelight')
+
+    args = parser.parse_args()
     #GAME = Game('127.0.0.1', 1337)
-    GAME = Game('ml.jaseg.net', 1337)
+    GAME = Game(
+        args.host,
+        args.port,
+        args.width,
+        args.height,
+    )
     GAME.gameloop()
